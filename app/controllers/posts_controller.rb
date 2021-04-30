75cb97a7e3b582f433daee0f9a6d0f66f5d37557
Goh Matsumoto <urimaro@gmail.com>
2021-04-30 14:02:48 +0900

    Get the post data in the controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,4 +19,8 @@ class PostsController < ApplicationController
   def edit
     @post = Post.find_by(id: params[:id])
   end
+
+  def update
+    redirect_to "/posts/index"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   post 'posts/create'
   get 'posts/:id' => 'posts#show'
   get 'posts/:id/edit' => 'posts#edit'
+  post 'posts/:id/update' => 'posts#update'
 
   get '/' => 'home#top'
   get 'about' => 'home#about'
