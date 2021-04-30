a06816e17d5075286de5c7564f53993a03984033
Goh Matsumoto <urimaro@gmail.com>
2021-04-30 17:56:29 +0900

    Delete a post

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'posts/:id' => 'posts#show'
   get 'posts/:id/edit' => 'posts#edit'
   post 'posts/:id/update' => 'posts#update'
+  post 'posts/:id/destroy' => 'posts#destroy'
 
   get '/' => 'home#top'
   get 'about' => 'home#about'
