772097dafbb50b6fb66e01c56ce8adaa47848b33
Goh Matsumoto <urimaro@gmail.com>
2021-05-02 12:35:36 +0900

    Validate on the new post page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,12 +8,16 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
     @post = Post.new(content: params[:content])
-    @post.save
-    redirect_to "/posts/index"
+    if @post.save
+      redirect_to "/posts/index"
+    else
+      render "posts/new"
+    end
   end
 
   def edit
