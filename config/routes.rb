e71ade0c5743621d60bf192d16ac376c8b4bb830
Goh Matsumoto <urimaro@gmail.com>
2021-05-06 01:46:59 +0900

    Update the user's changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'signup' => 'users#new'
   post 'users/create' => 'users#create'
   get 'users/:id/edit' => 'users#edit'
+  post 'users/:id/update' => 'users#update'
   get 'users/:id' => 'users#show'
 
   get 'posts/index'
