8fe90cb036f6abc9c4214f3cb503c2b55514dd13
Goh Matsumoto <urimaro@gmail.com>
2021-05-12 22:57:59 +0900

    Execute 'bin/rails db:migrate'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_09_135010) do
+ActiveRecord::Schema.define(version: 2021_05_12_134451) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_05_09_135010) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_name"
+    t.string "password"
   end
 
 end
