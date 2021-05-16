5baec3258b6e1b040cb24056415772628290cc75
Goh Matsumoto <urimaro@gmail.com>
2021-05-16 23:13:46 +0900

    Authenticate a user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,18 @@ class ApplicationController < ActionController::Base
   def set_current_user
     @current_user = User.find_by(id: session[:user_id])
   end
+
+  def authenticate_user
+    unless @current_user
+      flash[:notice] = "ログインが必要です"
+      redirect_to "/login"
+    end
+  end
+
+  def forbid_login_user
+    if @current_user
+      flash[:notice] = "すでにログインしています"
+      redirect_to "/posts/index"
+    end
+  end
 end
