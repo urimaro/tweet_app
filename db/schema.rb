01972a4ff0cac60dc6993372e8e6f7be48e3b524
Goh Matsumoto <urimaro@gmail.com>
2021-05-29 12:29:20 +0900

    Add 'password_digest' and remove 'password'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_25_020108) do
+ActiveRecord::Schema.define(version: 2021_05_29_032519) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2021_05_25_020108) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_name"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
